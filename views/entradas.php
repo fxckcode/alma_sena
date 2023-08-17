d8b7a6783ad3156f7798586179f4c260cde89611
fxckcode <diegoale8582@hotmail.com>
2023-08-17 11:24:51 -0500

    format

--- views/entradas.php
+++ views/entradas.php
@@ -225,7 +225,8 @@ include("../controllers/addElements.controller.php");
             <td><?= $tableData->observacion ?></td>
             <td class="d-flex flex-row gap-1">
               <!-- Botón editar -->
-              <a class="btn btn-small btn-warning" href="modificarElementos.php?id=<?= $tableData->idElemento ?>"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+              <a class="btn btn-small btn-warning" href="modificarElementos.php?id=<?= $tableData->idElemento ?>" data-id="<?= $tableData->idElemento ?>">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                   <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                   <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
                 </svg>
@@ -251,7 +252,7 @@ include("../controllers/addElements.controller.php");
   <script src="../csss/DataTables/datatables.min.js"></script>
   <script>
     new DataTable("#tableInventario")
-  </script> 
+  </script>
   <script src="../js/entradas.js"></script>
   <script src="../js/elementosGestionar.js"></script>
   <script src="../csss/bootstrap/js/bootstrap.min.js"></script>
